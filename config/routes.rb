efb46e0cafa3bcdab6f0f06bb8875c80fdeb89e5
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2023-08-15 23:58:01 -0300

    refactor view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # When using resources you have 7 types of differente routes or you can remove resource and specify a route you want.  
   resources :coins
   resources :departamentos
-  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+  get "produtos/busca", to: "produtos#b usca", as: :busca_produto
   resources :produtos
-  root to: "produtos#index"
+  
+  root to: 'welcome#index'
 end
